b8c747065b84006ef3c4dcccebeec22d79ea290c
Prabal <aryalparbal@gmail.com>
2024-05-01 19:49:18 +0545

    created a database for product stock and login information

--- stationary/application/models.py
+++ stationary/application/models.py
@@ -18,3 +18,43 @@ class stationaryitems(models.Model):
             {"product_name": product_name, "price": price, "quantity": quantity}
         )
         client.close()
+
+
+class productsstock(models.Model):
+    product_name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+
+    @staticmethod
+    def product(product_name, quantity):
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client["stationary"]
+        collection = db["collection2"]
+        collection.insert_one({"product_name": product_name, "quantity": quantity})
+        client.close()
+
+
+# Path: stationary/application/views.py
+# class login_info(models.Model):
+#   username = models.CharField(max_length=100)
+#  password = models.CharField(max_length=100)
+
+# @staticmethod
+# def login(username, password):
+#   client = pymongo.MongoClient("mongodb://localhost:27017/")
+#  db = client["stationary"]
+# collection = db["collection3"]
+# collection.insert_one({"username": username, "password": password})
+# client.close()
+
+
+class register_info(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    @staticmethod
+    def register(username, password):
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client["stationary"]
+        collection = db["collection4"]
+        collection.insert_one({"username": username, "password": password})
+        client.close()
